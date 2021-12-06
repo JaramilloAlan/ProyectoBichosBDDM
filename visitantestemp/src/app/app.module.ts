391bdb564e1424f8d3b879b1b1cb219ec2f7ca72
JaramilloAlan <78992088+JaramilloAlan@users.noreply.github.com>
2021-12-06 14:50:38 -0600

    Add files via upload

---
+++ visitantestemp/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RegistroComponent } from './registro/registro.component';
+import { TarjetitasComponent } from './tarjetitas/tarjetitas.component';
+import { TablasComponent } from './tablas/tablas.component';
+import {ReactiveFormsModule} from "@angular/forms";
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    RegistroComponent,
+    TarjetitasComponent,
+    TablasComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
